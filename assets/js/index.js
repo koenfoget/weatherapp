1d558a6e1dbc7e64d74db4bb4c7a66b0dc4e8a53
koenfoget <koenfoget@gmail.com>
2020-02-19 11:16:55 +0100

    Add localstorage

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,6 +4,7 @@ function getCurrent() {
     } else {
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
+    loadLocalStorageItems()
 }
 
 function fetchAll(url, current = false) {
@@ -12,7 +13,7 @@ function fetchAll(url, current = false) {
             if (response.ok) {
                 response.json().then(function (data) {
                     var div = document.createElement('div');
-                    div.classList.add( 'city-block');
+                    div.classList.add('city-block');
                     // var wtemp = document.getElementsByClassName("temp_display");
                     // var weather = document.getElementsByClassName("weather_display");
                     // var location = document.getElementsByClassName("location_display");
@@ -50,7 +51,7 @@ function showPosition(position) {
     t = position.coords.latitude;
     n = position.coords.longitude;
 
-    const url='http://api.openweathermap.org/data/2.5/weather?lat=' + t + '&lon=' + n + '&appid=d518e802e2b3693fb0123bb620f7d420&units=metric';
+    const url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + t + '&lon=' + n + '&appid=d518e802e2b3693fb0123bb620f7d420&units=metric';
     fetchAll(url, true);
 
 }
@@ -66,10 +67,38 @@ function getLocation() {
         const url = 'http://api.openweathermap.org/data/2.5/weather?q=' + value + '&appid=d518e802e2b3693fb0123bb620f7d420&units=metric';
         console.log(url);
         fetchAll(url);
+        saveLocalStorageItem(value)
     });
 }
 
-
+// Save the city name to the localStorage
+function saveLocalStorageItem(value) {
+    // Get the current weateritems
+    let weatheritem = JSON.parse(localStorage.getItem('cityName'));
+    // If there is no weatheritems, create new Array
+    if (!weatheritem) {
+        weatheritem = [];
+    }
+    // Add the cityname to the array
+    weatheritem.push(value);
+    // Set the item
+    localStorage.setItem('cityName', JSON.stringify(weatheritem));
+}
+function loadLocalStorageItems() {
+    // Get the weatheritems
+    const weatheritem = JSON.parse(localStorage.getItem('cityName'));
+    // If there are no weatheritems, do nothing
+    if (!weatheritem) {
+        return false;
+    }
+    // Do something for each item
+    weatheritem.forEach(function(value) {
+        // Call the API to get the data
+        const url = 'http://api.openweathermap.org/data/2.5/weather?q=' + value + '&appid=d518e802e2b3693fb0123bb620f7d420&units=metric';
+            // Call the fetch to generate Added weather based on the data
+            fetchAll(url);
+    });
+}
 
 getCurrent();
 getLocation();
